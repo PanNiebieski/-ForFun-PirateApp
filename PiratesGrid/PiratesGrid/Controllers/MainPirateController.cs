6f72c294e4cd06b93f4b5b3e3030c5cf6959f188
Cezary Walenciuk <cezarywalenciuk@gmail.com>
2020-02-25 09:36:47 +0100

    Adding first Controllers

---
+++ PiratesGrid/PiratesGrid/Controllers/MainPirateController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PiratesGrid.Services;
+
+namespace PiratesGrid.Controllers
+{
+    [Route("Home")]
+    public class MainPirateController : Controller
+    {
+        private IPirateService _pirateService;
+
+        public MainPirateController(IPirateService pirateService)
+        {
+            _pirateService = pirateService;
+        }
+
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- PiratesGrid/PiratesGrid/Startup.cs
+++ PiratesGrid/PiratesGrid/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PiratesGrid.Services;
 
 namespace PiratesGrid
 {
@@ -16,6 +17,13 @@ namespace PiratesGrid
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddControllersWithViews();
+            services.AddRouting(options => options.LowercaseUrls = true);
+
+            services.AddSingleton<IPirateService, PirateMemoryService>();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +34,27 @@ namespace PiratesGrid
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+            //app.UseAuthentication();
+            app.UseHttpsRedirection();
+
             app.UseRouting();
+
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                    );
             });
         }
     }
